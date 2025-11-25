f0d79daf590dca59d02c61f662e6ce16b1a0ab30
Accesito24 <brayandavidg24@gmail.com>
2025-11-25 20:56:27 +0100

    Agregar ejercicio de contar palabras

---
+++ src/Strings/ContarCaracter.java
@@ -0,0 +1,18 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class ContarCaracter {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Ingrese una frase:");
+        String frase = leer.nextLine();
+
+        System.out.println("Ingrese un caracter y te dire cuantas veces aparece este caracter:");
+        String caracter = leer.nextLine();
+
+
+    }
+
+}

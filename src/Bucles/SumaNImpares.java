51a566c5348256911160a523379d6caecafe6c06
Accesito24 <brayandavidg24@gmail.com>
2025-11-25 15:24:09 +0100

    mensaje del commit

--- src/Bucles/SumaNImpares.java
+++ src/Bucles/SumaNImpares.java
@@ -1,4 +1,25 @@
 package Bucles;
 
+import java.util.Scanner;
+
 public class SumaNImpares {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+
+
+        System.out.print("Dame un numero entero positivo y te mostrare");
+        System.out.print(" los numeros impares desde el 1 hasta el numero");
+        System.out.print(" que me digas");
+        
+        int num1 = leer.nextInt();
+        
+        if (num1 <= 0){
+            System.out.println("El numero es negativo");
+        } else {
+
+            
+        }
+
+
+    }
 }
--- src/EntradaYsalidas/CalculadoraSimple.java
+++ src/EntradaYsalidas/CalculadoraSimple.java
@@ -4,9 +4,33 @@ import java.util.Scanner;
 
 public class CalculadoraSimple {
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
 
+        System.out.println("=== CALCULADORA SIMPLE ===");
+        System.out.println("Elige una opción:");
+        System.out.println("1. Con variables");
+        System.out.println("2. Sin variables");
+        System.out.print("Tu elección: ");
 
+        int opcion = leer.nextInt();
+
+        switch(opcion) {
+            case 1:
+                Variables();
+                break;
+            case 2:
+                SinVariables();
+                break;
+            default:
+                System.out.println("Opción no válida");
+        }
+
+        leer.close();
+
+    }
+
+    public static void Variables() {
         Scanner leer = new Scanner(System.in);
 
         System.out.println("Dame dos numero y te dire el resultado de la suma, resta y multiplicacion");
@@ -23,16 +47,25 @@ public class CalculadoraSimple {
         System.out.println("la suma es: " + suma + " de " + num1 + " y " + num2 );
 
         //print es una sola linea hasta que se le de el cierre, se puede usar cuantas veces quiera el print
-       // se usa "\n" al final de la linea
+        // se usa "\n" al final de la linea
         System.out.print("la resta es: "  + resta);
         System.out.print(" de los numero: " + num1 + " y " + num2 + "\n");
 
         System.out.println("la multiplicacion es: " + multiplicacion);
 
+        leer.close();
 
+    }
 
+    public static void SinVariables() {
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Dame dos numero y te dire el resultado de la suma, resta y multiplicacion");
+        double num1 = leer.nextDouble();
+        double num2 = leer.nextDouble();
 
+        System.out.println("El resultado de la suma de: " + num1 + " y " + num2 + " es: " + (num1 + num2));
 
+        leer.close();
 
     }
 }
